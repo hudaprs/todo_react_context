5b2729a07c57f9bafb8fb86875c9feb975e59db8
Huda Prasetyo <huda.prs2002@gmail.com>
2020-08-08 09:58:20 +0700

    first commit and completing the app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
+// Styles
+import "./style.css"
+
+// Components
+import TodoForm from "./components/TodoForm"
+import TodoList from "./components/TodoList"
+
+// State
+import TodoState from "./context/TodoState"
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // Wrap entire app with State
+    <TodoState>
+      <div className='container flex flex-col mt-4'>
+        <h1 className='text-center'>Todo App With Context</h1>
+
+        {/* Todo Form */}
+        <TodoForm />
+
+        {/* Todo List */}
+        <div className='flex flex-col mt-4'>
+          <TodoList />
+        </div>
+      </div>
+    </TodoState>
+  )
 }
 
-export default App;
+export default App
